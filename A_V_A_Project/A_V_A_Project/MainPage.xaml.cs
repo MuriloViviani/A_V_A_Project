991f90cc69539e368c91e3bea2b628c6d0482fcc
murilo viviani <muriloviviani@hotmail.com>
2017-10-01 17:00:06 -0300

    Add project files.

---
+++ A_V_A_Project/A_V_A_Project/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace A_V_A_Project
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
